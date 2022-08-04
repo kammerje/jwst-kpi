ee36518a796103b1bfe06e8e8d90e6cc6b3241ec
Thomas Vandal <thomasvandal@hotmail.com>
2022-08-04 01:56:56 -0400

    Add pupil mask and model fits files

--- jwst_kernel/__init__.py
+++ jwst_kernel/__init__.py
@@ -1,5 +1,10 @@
 from pkg_resources import get_distribution, DistributionNotFound
+
+from jwst_kernel import pupil_data
+
 try:
     __version__ = get_distribution(__name__).version  # get version from setup
 except DistributionNotFound:
     pass  # package is not installed
+
+PUPIL_DIR = pupil_data.__path__[0]
