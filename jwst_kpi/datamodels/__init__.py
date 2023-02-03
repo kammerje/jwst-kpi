445b1c12ba5cfc3982cee5f3833c7b1af6126c64
Thomas Vandal <thomasvandal@hotmail.com>
2023-02-03 15:12:51 -0500

    Add all datamodels to init class

--- jwst_kpi/datamodels/__init__.py
+++ jwst_kpi/datamodels/__init__.py
@@ -1,5 +1,13 @@
+from .badpixcube import BadPixCubeModel
+from .kpfits import KPFitsModel
+from .recentercube import RecenterCubeModel
 from .trimmedcube import TrimmedCubeModel
+from .windowcube import WindowCubeModel
 
 __all__ = [
+    "BadPixCubeModel",
     "TrimmedCubeModel",
+    "RecenterCubeModel",
+    "WindowCubeModel",
+    "KPFitsModel",
 ]
