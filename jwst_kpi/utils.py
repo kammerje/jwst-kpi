22d32fd666e822d8bea2737391ec5460b085d6cc
Thomas Vandal <thomasvandal@hotmail.com>
2023-02-04 23:31:54 -0500

    Add utility function to remove Kpi3 suffixes
    Similar to the one for JWST models in JWST pipeline, but that knows
    about KPI pipeline steps

--- jwst_kpi/utils.py
+++ jwst_kpi/utils.py
@@ -1,29 +1,38 @@
 from __future__ import division
 
+import re
+
 import matplotlib
-matplotlib.rcParams.update({'font.size': 14})
+
+matplotlib.rcParams.update({"font.size": 14})
 
 
 # =============================================================================
 # IMPORTS
 # =============================================================================
 
-import os
-import pdb
-import sys
-
-import astropy.io.fits as pyfits
-import matplotlib.pyplot as plt
-import numpy as np
-
-from astropy.io.fits.hdu.hdulist import HDUList
 from pathlib import Path
 from typing import Optional, Union
 
+import astropy.io.fits as pyfits
+
+KPI_SUFFIXES = [
+    "trimframesstep",
+    "fixbadpixelsstep",
+    "recenterframesstep",
+    "windowframesstep",
+    "extractkerphasestep",
+    "empiricaluncertaintiesstep",
+]
+
+REMOVE_SUFFIX_REGEX_KPI = re.compile(
+    "^(?P<root>.+?)((?P<separator>_|-)(" + "|".join(KPI_SUFFIXES) + "))?$"
+)
 
 # =============================================================================
 # MAIN
 # =============================================================================
+
 
 def split_file_path(
     file: Union[Path, str],
@@ -41,6 +50,7 @@ def split_file_path(
 
     return parent_dir, file_stem, fext
 
+
 def open_fits(
     file: Union[Path, str],
     suffix: Optional[str] = None,
@@ -50,13 +60,14 @@ def open_fits(
     file_path = Path(file)
     suffix = suffix or ""
     parent_dir, file_stem, fext = split_file_path(file_path)
-    basename = file_stem + suffix + fext # handle compressed files, e.g., fits.gz
+    basename = file_stem + suffix + fext  # handle compressed files, e.g., fits.gz
     if file_dir is None:
         file_path = Path(parent_dir) / basename
     else:
         file_path = Path(file_dir) / basename
 
     return pyfits.open(file_path)
+
 
 def get_output_base(
     file: Union[Path, str],
@@ -71,3 +82,16 @@ def get_output_base(
         output_base = Path(output_dir) / file_stem
 
     return str(output_base)
+
+
+def remove_suffix_kpi(name):
+    separator = None
+    match = REMOVE_SUFFIX_REGEX_KPI.match(name)
+    try:
+        name = match.group('root')
+        separator = match.group('separator')
+    except AttributeError:
+        pass
+    if separator is None:
+        separator = '_'
+    return name, separator
