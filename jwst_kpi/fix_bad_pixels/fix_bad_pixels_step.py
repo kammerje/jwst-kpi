d835d9d71c2a5ba760cc2b549087eccda0a02213
Thomas Vandal <thomasvandal@hotmail.com>
2023-02-06 08:31:55 -0500

    Import dq flag dictionary from jwst pipeline
    Instead of hardcoding values in bad pixel step

--- jwst_kpi/fix_bad_pixels/fix_bad_pixels_step.py
+++ jwst_kpi/fix_bad_pixels/fix_bad_pixels_step.py
@@ -10,39 +10,7 @@ from scipy.ndimage import median_filter
 from .. import utils as ut
 from ..datamodels import BadPixCubeModel
 from .fix_bad_pixels_plots import plot_badpix
-
-# Bad pixel flags.
-# https://jwst-reffiles.stsci.edu/source/data_quality.html
-# TODO: import from jwst pipeline?
-pxdq_flags = {
-    "DO_NOT_USE": 1,
-    "SATURATED": 2,
-    "JUMP_DET": 4,
-    "DROPOUT": 8,
-    "UNRELIABLE_ERROR": 256,
-    "NON_SCIENCE": 512,
-    "DEAD": 1024,
-    "HOT": 2048,
-    "WARM": 4096,
-    "LOW_QE": 8192,
-    "RC": 16384,
-    "TELEGRAPH": 32768,
-    "NONLINEAR": 65536,
-    "BAD_REF_PIXEL": 131072,
-    "NO_FLAT_FIELD": 262144,
-    "NO_GAIN_VALUE": 524288,
-    "NO_LIN_CORR": 1048576,
-    "NO_SAT_CHECK": 2097152,
-    "UNRELIABLE_BIAS": 4194304,
-    "UNRELIABLE_DARK": 8388608,
-    "UNRELIABLE_SLOPE": 16777216,
-    "UNRELIABLE_FLAT": 33554432,
-    "OPEN": 67108864,
-    "ADJ_OPEN": 134217728,
-    "UNRELIABLE_RESET": 268435456,
-    "MSA_FAILED_OPEN": 536870912,
-    "OTHER_BAD_PIXEL": 1073741824,
-}
+from jwst.datamodels.dqflags import pixel as pxdq_flags
 
 
 class FixBadPixelsStep(Step):
