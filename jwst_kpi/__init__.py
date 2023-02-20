859a985101c1cf544b8565303898cf9cbd0e7445
Thomas Vandal <thomasvandal@hotmail.com>
2023-02-20 09:07:46 -0500

    Fix PUPIL_DIR import in package init file
    Import from `constants.py` instead of `calwebb_kpi3.py`

--- jwst_kpi/__init__.py
+++ jwst_kpi/__init__.py
@@ -1,6 +1,7 @@
 from pkg_resources import DistributionNotFound, get_distribution
 
-from .pipeline.calwebb_kpi3 import Kpi3Pipeline, PUPIL_DIR
+from .pipeline.calwebb_kpi3 import Kpi3Pipeline
+from .constants import PUPIL_DIR
 
 try:
     __version__ = get_distribution(__name__).version  # get version from setup
