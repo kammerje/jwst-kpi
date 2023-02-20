2abaa7ef1a3be3ae0dfde425d2eecda6757a7160
Thomas Vandal <thomasvandal@hotmail.com>
2023-02-20 08:46:58 -0500

    Test bad pixels with fourier

--- tests/test_badpix_step.py
+++ tests/test_badpix_step.py
@@ -3,12 +3,13 @@ import os
 from jwst_kpi.fix_bad_pixels.fix_bad_pixels_step import FixBadPixelsStep
 
 input_file = "tests/outputs/jw01093011001_03103_00001_nis_trimframesstep.fits"
-output_dir = "tests/outputs/"
+output_dir = "tests/outputs_fourierbp/"
 os.makedirs(output_dir, exist_ok=True)
 
 step = FixBadPixelsStep()
 step.show_plots = True
 step.save_results = True
+step.method = "fourier"
 step.output_dir = output_dir
 output = step.run(input_file)
 print(output)
