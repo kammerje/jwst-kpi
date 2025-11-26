69b7f6f8527cae97d5038fddd29d3a526c4b8db5
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-11-26 14:22:26 -0500

    Extract SVO fetching to a function

--- jwst_kpi/constants.py
+++ jwst_kpi/constants.py
@@ -27,35 +27,23 @@ gain = {
     "MIRI": 4.0,  # e-/ADU
 }
 
-# TODO: Convert to function?
 # Load the NIRCam, NIRISS, and MIRI filters from the SVO Filter Profile
 # Service.
 # http://svo2.cab.inta-csic.es/theory/fps/
-wave_nircam = {}
-weff_nircam = {}
-filter_list = SvoFps.get_filter_list(facility="JWST", instrument="NIRCAM")
-for i in range(len(filter_list)):
-    name = filter_list["filterID"][i]
-    name = name[name.rfind(".") + 1 :]
-    wave_nircam[name] = filter_list["WavelengthMean"][i] / 1e4  # micron
-    weff_nircam[name] = filter_list["WidthEff"][i] / 1e4  # micron
-wave_niriss = {}
-weff_niriss = {}
-filter_list = SvoFps.get_filter_list(facility="JWST", instrument="NIRISS")
-for i in range(len(filter_list)):
-    name = filter_list["filterID"][i]
-    name = name[name.rfind(".") + 1 :]
-    wave_niriss[name] = filter_list["WavelengthMean"][i] / 1e4  # micron
-    weff_niriss[name] = filter_list["WidthEff"][i] / 1e4  # micron
-wave_miri = {}
-weff_miri = {}
-filter_list = SvoFps.get_filter_list(facility="JWST", instrument="MIRI")
-for i in range(len(filter_list)):
-    name = filter_list["filterID"][i]
-    name = name[name.rfind(".") + 1 :]
-    wave_miri[name] = filter_list["WavelengthMean"][i] / 1e4  # micron
-    weff_miri[name] = filter_list["WidthEff"][i] / 1e4  # micron
-del filter_list
+def get_wave(instrument: str):
+    wave = {}
+    weff = {}
+    filter_list = SvoFps.get_filter_list(facility="JWST", instrument=instrument.upper())
+    for i in range(len(filter_list)):
+        name = filter_list["filterID"][i]
+        name = name[name.rfind(".") + 1 :]
+        wave[name] = filter_list["WavelengthMean"][i] / 1e4  # micron
+        weff[name] = filter_list["WidthEff"][i] / 1e4  # micron
+    return wave, weff
+
+wave_nircam, weff_nircam = get_wave("NIRCAM")
+wave_niriss, weff_niriss = get_wave("NIRISS")
+wave_miri, weff_miri = get_wave("MIRI")
 
 WRAD_DEFAULT = 24
 
